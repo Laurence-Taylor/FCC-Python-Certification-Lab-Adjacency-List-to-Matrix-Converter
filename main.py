c671d723bfdc8e9e688eeac068a95069b37d0e82
LaurenceTaylor <morfeus06388casa@gmail.com>
2026-01-27 20:15:08 -0600

    feat:final version

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 def adjacency_list_to_matrix(adj_list):
+    # Determine the number of nodes
     matrix_len = len(adj_list)
+    # Create a new Matrix with all values in 0
     matrix_to_return = [[0 for _ in range(matrix_len)] for _ in range(matrix_len)]
+    # counter of nodes
     i = 0
-    j = 0
+    # for each dictioanry key value pair (items)
     for key, list_edge in adj_list.items():
+        # for each value in the list
         for value in list_edge:
             matrix_to_return[i][value]=1
+        # print matrix row
+        print(matrix_to_return[i])
+        # next node
         i += 1
-    print(matrix_to_return)
+    # return matix
+    return matrix_to_return
 
 if __name__ == '__main__':
     adjacency_list_to_matrix({0: [2], 1: [2, 3], 2: [0, 1, 3], 3: [1, 2]})
